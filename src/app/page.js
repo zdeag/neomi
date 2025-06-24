6f3e6b321498927028df6d272d892ea9af511ee6
zdeag <deaguiarzachary@gmail.com>
2025-06-24 14:31:01 -0400

    countdown

--- src/app/page.js
+++ src/app/page.js
@@ -2,5 +2,8 @@ import Timer from "./Timer";
 
 export default function Home() {
   return (
+    <main className="min-h-screen flex items-center justify-center bg-transparent">
+      <Timer />
+    </main>
   );
 }
